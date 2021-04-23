12b18e5997fdd29dc1e38c781c30f1c1a8331a8c
Matheus Luis <suetham_luis@hotmail.com>
2021-04-23 00:05:39 -0300

    Fist Commit

--- lambda.go
+++ lambda.go
@@ -10,13 +10,17 @@ type APIGatewayEvent struct {
 	Name string `json:"name"`
 }
 
+type HttpHelper struct {
+	Success bool `json:"success"`
+}
+
 type APIGatewayEventResponse struct {
-	StatusCode int    `json:"statusCode"`
-	Body       string `json:"body"`
+	StatusCode int        `json:"statusCode"`
+	Body       HttpHelper `json:"body"`
 }
 
 func HandleRequest(ctx context.Context, name APIGatewayEvent) (APIGatewayEventResponse, error) {
-	return APIGatewayEventResponse{StatusCode: 200, Body: name.Name}, nil
+	return APIGatewayEventResponse{StatusCode: 200, Body: HttpHelper{Success: true}}, nil
 }
 
 func main() {
