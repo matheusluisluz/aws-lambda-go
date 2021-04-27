3d2668eec4663bd0d8dc30b36b4b36c4f0612e57
Matheus Luis <suetham_luis@hotmail.com>
2021-04-27 20:49:26 -0300

    Adjustment in structs

--- lambda.go
+++ lambda.go
@@ -12,15 +12,15 @@ type APIGatewayEvent struct {
 	Name string `json:"name"`
 }
 
-type HttpHelper struct {
+type Response struct {
 	Success bool `json:"success"`
 }
 
 func HandleRequest(ctx context.Context, event events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	body := HttpHelper{Success: true}
+	body := Response{Success: true}
 	response, err := json.Marshal(body)
 	if err != nil {
-		errorResponse, _ := json.Marshal(HttpHelper{Success: false})
+		errorResponse, _ := json.Marshal(Response{Success: false})
 		return events.APIGatewayProxyResponse{StatusCode: 500, Body: string(errorResponse)}, nil
 	}
 	return events.APIGatewayProxyResponse{StatusCode: 200, Body: string(response)}, nil
